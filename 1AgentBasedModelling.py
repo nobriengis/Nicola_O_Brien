5ddc87bee7006057867537eb1a4595220b8924b1
nobriengis <53270083+nobriengis@users.noreply.github.com>
2021-02-12 14:39:57 +0000

    Update 1AgentBasedModelling.py
    Updated 2021

--- 1AgentBasedModelling.py
+++ 1AgentBasedModelling.py
@@ -1,155 +1,90 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Sep 12 09:40:20 2019
+Created on Tue Jan 12 12:40:20 2021
+
 
 @author: nobrien
 """
-import random
-
-# set up variables
-y0=50
-x0=50
-
-# Random walk one step
-
-if random.random() < 0.5:
-    y0 += 1
-else:
-    y0 -= 1
-    
-print(y0) 
 
 import random
 
-# set up variables
 
-y0 = 50
-x0 = 50
+#set up variables y0 and x0 to be random integers between 0 and 99
+y0=random.randint(0,99)
+x0=random.randint(0,99)
 
-# Random walk one step
+print(y0,x0)
 
+#use if statement to move y one step
 if random.random() < 0.5:
-    y0 += 1
+    y0=y0+1
 else:
-    y0 -= 1
+    y0=y0-1
 
+
+#use if statement to move x one step
 if random.random() < 0.5:
-    x0 += 1
+    x0=x0+1
 else:
-    x0 -= 1
+    x0=x0-1
 
+print(y0,x0)
+
+#use if statement to move y another one step
 if random.random() < 0.5:
-    y0 += 1
+    y0=y0+1
 else:
-    y0 -= 1
+    y0=y0-1
 
+
+#use if statement to move x another one step
 if random.random() < 0.5:
-    x0 += 1
+    x0=x0+1
 else:
-    x0 -= 1
+    x0=x0-1
 
-print(y0, x0)
+print(y0,x0)
 
-# set up variables
 
-y1 = 50
-x1 = 50
+#set up variables y1 and x1 to be random intergers between 0 and 99
+y1=random.randint(0,99)
+x1=random.randint(0,99)
 
-# Random walk one step
+print(y1,x1)
 
+#use if statement to move y one step
 if random.random() < 0.5:
-    y1 += 1
+    y1+=1
 else:
-    y1 -= 1
+    y1-=1
+   
 
+#use if statement to move x one step
 if random.random() < 0.5:
-    x1 += 1
+    x1+=1
 else:
-    x1 -= 1
+    x1-=1  
 
-if random.random() < 0.5:
-    y1 += 1
-else:
-    y1 -= 1
+print(y1,x1)
 
+#use if statement to move y another one step
 if random.random() < 0.5:
-    x1 += 1
+    y1+=1
 else:
-    x1 -= 1
+    y1-=1
     
-print(y1, x1)
 
-y_diff = (y0 - y1)
-y_diffsq = y_diff * y_diff
-x_diff = (x0 - x1)
-x_diffsq = x_diff * x_diff
-sum = y_diffsq + x_diffsq
-answer = sum**0.5
-print(answer)
-
-#random number between 0 and 99
-
-y0 = random.randint(0,99)
-x0 = random.randint(0,99)
-
-
-# Random walk one step
-
+#use if statement to move x another one step
 if random.random() < 0.5:
-    y0 += 1
+    x1+=1
 else:
-    y0 -= 1
-
-if random.random() < 0.5:
-    x0 += 1
-else:
-    x0 -= 1
-
-if random.random() < 0.5:
-    y0 += 1
-else:
-    y0 -= 1
-
-if random.random() < 0.5:
-    x0 += 1
-else:
-    x0 -= 1
-
-print(y0, x0)
-
-# set up variables
-
-y1 = random.randint(0,99)
-x1 = random.randint(0,99)
-
-# Random walk one step
-
-if random.random() < 0.5:
-    y1 += 1
-else:
-    y1 -= 1
-
-if random.random() < 0.5:
-    x1 += 1
-else:
-    x1 -= 1
-
-if random.random() < 0.5:
-    y1 += 1
-else:
-    y1 -= 1
-
-if random.random() < 0.5:
-    x1 += 1
-else:
-    x1 -= 1
+    x1-=1
     
-print(y1, x1)
 
-y_diff = (y0 - y1)
-y_diffsq = y_diff * y_diff
-x_diff = (x0 - x1)
-x_diffsq = x_diff * x_diff
-sum = y_diffsq + x_diffsq
-answer = sum**0.5
-print(answer)
+print(y1,x1)
+
+#Calculate the distance between the 2 agents (y0,x0) and (y1,x1)
+
+dist=(((y1-y0)**2) + ((x1-x0)**2))**0.5
+
+print(dist)
